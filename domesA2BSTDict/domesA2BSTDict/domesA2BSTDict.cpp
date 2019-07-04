e50be8673dcb75541c5c805a60ff7ad4020fca52
DimPap99 <33203177+DimPap99@users.noreply.github.com>
2019-07-04 03:00:53 +0300

    Add files via upload

--- domesA2BSTDict/domesA2BSTDict/domesA2BSTDict.cpp
+++ domesA2BSTDict/domesA2BSTDict/domesA2BSTDict.cpp
@@ -4,6 +4,10 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <list>
+#include <sstream>
+
+#include <Windows.h>
 
 using namespace std;
 //Το δυαδικο δεντρο αναζήτησης θα περιέχει κομβους.
@@ -12,6 +16,7 @@ using namespace std;
 class Node{
 	public:
 		int nodeLevel = 1;
+		
 		string word = "";
 		string translation = "";
 		Node *leftChild = NULL;
@@ -27,8 +32,97 @@ class Node{
 //To BST αποτελείται από κόμβους της κλάσης Node.
 class BinarySearchTree {
 	public:
+		//αποθηκευεται το μεγιστο υψος καθε φορα που γινεται insert
+		int maxLevel = 1;
+		std::list<string> toPrint;
+		int totalNodes = 0;
 		Node *root = NULL;
+		
+		int printOrder = 0;
 
+
+		void printLevelOrder(Node* root)
+		{
+			int h = height(root);
+			int i;
+			for (i = 1; i <= h; i++) {
+				printGivenLevel(root, i);
+				cout << endl;
+			}
+		}
+
+		int height(Node* node)
+		{
+			if (node == NULL)
+				return 0;
+			else
+			{
+				/* compute the height of each subtree */
+				int lheight = height(node->leftChild);
+				int rheight = height(node->rightChild);
+
+				/* use the larger one */
+				if (lheight > rheight)
+					return(lheight + 1);
+				else return(rheight + 1);
+			}
+		}
+
+
+		void printGivenLevel(Node* root, int level)
+		{
+			if (root == NULL) {
+
+				return;
+			}
+			if (level == 1) {
+				
+				cout << root->word;
+			//	if (level > 2) p - 1;
+				
+			}
+			else if (level > 1)
+			{
+				
+				printGivenLevel(root->leftChild, level - 1);
+				printGivenLevel(root->rightChild, level - 1);
+			}
+		}
+
+
+
+		Node *search(string word) {
+			list<Node *>words;
+			this->Inorder(this->root, words, word);
+			cout << "The words that match are:\n";
+			for (auto v : words){
+				std::cout << v->word << "\n";
+		}	
+			string wordToReturn;
+			cout << "Choose for which word you want to see the translation"<<endl;
+			cin >> wordToReturn;
+			Node *n = NULL;
+			n = this->root;
+			while (n != NULL)
+			{
+				if (n->word == wordToReturn) {
+					cout << n->translation;
+					return n;
+				}
+				if (n != NULL) {
+					if (wordToReturn < n->word) {
+						n = n->leftChild;
+					}
+				}
+				if (n != NULL) {
+					if (wordToReturn > n->word) {
+						n = n->rightChild;
+					}
+				}
+			}
+			cout <<"Word not found" ;
+			return n;
+		}
 		bool insert(string word, string translation) {
 
 			//if the rood in NULL the we add the word and its translation 
@@ -38,6 +132,7 @@ class BinarySearchTree {
 				root->word = word;
 				root->translation = translation;
 				this->root = root;
+				this->totalNodes++;
 				return true;
 			}
 			int countlevels = 1;
@@ -53,11 +148,16 @@ class BinarySearchTree {
 				//Αν η λεξη ειναι μικροτερη αλφαβητικά της λέξης του κομβού συνεχίζω και δεν υπαρχει αριστερο παιδι
 				//το δημιουργω και αποθηκευω την λεξη
 				if (word < n->word && n->word != "" && n->leftChild == NULL) {
+					countlevels++;
 					Node *leftChild = NULL;
 					leftChild = new Node();
 					leftChild->word = word;
 					leftChild->translation = translation;
 					leftChild->nodeLevel = countlevels;
+					if (countlevels > this->maxLevel) {
+						maxLevel = countlevels;
+					}
+					this->totalNodes++;
 					n->leftChild = leftChild;
 					return true;
 				}
@@ -69,11 +169,16 @@ class BinarySearchTree {
 				
 				}
 				if (word > n->word && n->word != "" && n->rightChild == NULL) {
+					countlevels++;
 					Node *rightChild = NULL;
 					rightChild = new Node();
 					rightChild->word = word;
 					rightChild->translation = translation;
 					rightChild->nodeLevel = countlevels;
+					if (countlevels > this->maxLevel) {
+						maxLevel = countlevels;
+					}
+					this->totalNodes++;
 					n->rightChild = rightChild;
 					return true;
 
@@ -97,6 +202,9 @@ class BinarySearchTree {
 				 Node *n = NULL;
 				 Node *prevNode = NULL;
 			while (subtreeRoot != NULL) {
+				if (subtreeRoot->rightChild != NULL && subtreeRoot->rightChild->leftChild != NULL) {
+
+				}
 				if (subtreeRoot->leftChild == NULL && subtreeRoot->rightChild == NULL) {
 					n = new Node();
 					n->word = subtreeRoot->word;
@@ -119,10 +227,12 @@ class BinarySearchTree {
 			}
 			
 			
-			//delete subtreeRoot;
+			//Αν η επαναληψη δεν μπει στο if τοτε σημαινει οτι δεν υπαρχει καποιο αριστερο φυλο στους
+			//κομβους που εχουν κοντινες τιμες με τον κομβο που διαγραφεται
+			//οποτε ο πιο κοντινος σε τιμη κομβος θα ειναι το δεξι παιδι και αφου δεν υπαρχει αριστερο η επαναληψη σταματα
 			return n;
 		}
-			 Node * minValueNode( Node* node)
+			 Node * minValueNode(Node* node)
 			 {
 				 Node* current = node;
 
@@ -132,9 +242,8 @@ class BinarySearchTree {
 
 				 return current;
 			 }
-			 /* Given a binary search tree and a key, this function deletes the key
-			 and returns the new root */
-			 struct Node* deleteNode( Node* root, string word)
+
+			  Node* deleteNode(Node* root, string word)
 			 {
 				 // base case 
 				 if (root == NULL) return root;
@@ -157,13 +266,13 @@ class BinarySearchTree {
 					 if (root->leftChild == NULL)
 					 {
 						 Node *temp = root->rightChild;
-						 free(root);
+						// free(root);						
 						 return temp;
 					 }
 					 else if (root->rightChild == NULL)
 					 {
 						 Node *temp = root->leftChild;
-						 free(root);
+						 //free(root);
 						 return temp;
 					 }
 
@@ -179,15 +288,35 @@ class BinarySearchTree {
 				 }
 				 return root;
 			 }
-		void printInorder(Node *root) {
+
+			  
+
+
+
+
+
+
+
+
+
+		
+		void Inorder(Node *root,list<Node *> &wordList,string word) {
 
 			if (root == NULL) {
 				return;
 			}
-
-			printInorder(root->leftChild);
-			cout << root->word << " ";
-			printInorder(root->rightChild);
+			
+			Inorder(root->leftChild,wordList,word);
+			
+			if (root->word.rfind(word, 0) == 0) {
+				wordList.push_back(root);
+				// s starts with prefix
+			}
+			//cout<<root->word + " " ; 
+			
+			
+			Inorder(root->rightChild, wordList, word);
+			
 
 		}
 			
@@ -197,22 +326,35 @@ class BinarySearchTree {
 
 int main()
 {
+	
+	
 	BinarySearchTree bst;
-	bst.insert("a", "A");
-	bst.insert("e", "E");
-	//bst.insert("d", "E");
-	bst.insert("g", "G");
-	bst.insert("f", "f");
-	//bst.insert("c", "C");
+	//bst.insert("a", "A");
+	bst.insert("cat", "gata");
+	bst.insert("air", "aeras");
+	bst.insert("dub", "adexios");
+	//bst.insert("b", "B");
+	bst.insert("egg", "augo");
+	bst.insert("dust", "skoni");
+	bst.insert("dumb", "xazos");
+//	bst.insert("g", "G");
+	bst.insert("fun", "asteios");
+	bst.insert("dunk", "karfwnw");
+	//cout<<bst.search("f")->word;
+	
 	//bst.insert("b", "B");
 	/*bst.insert("c", "C");
 	bst.insert("e", "E");
 	bst.insert("g", "G");
 	bst.insert("f", "F");
-	*/bst.printInorder(bst.root);
+	*/
+	
+	bst.search("du");
+	//bst.printInorder(bst.root);
 	cout << endl;
-	bst.deleteNode(bst.root,"e");
-	bst.printInorder(bst.root);
+	bst.root = bst.deleteNode(bst.root,"dunk");
+	bst.search("du");
+	//bst.printInorder(bst.root);
     return 0;
 }
 
